d73dc2ce582ed5a1e40f33848f3be9b5b4b643ac
stfn_ko <stepan.kolontaj@gmail.com>
2024-04-11 21:23:10 +0300

    update

--- src/roninc/parser/mod.rs
+++ src/roninc/parser/mod.rs
@@ -1,4 +1,6 @@
+use super::lexer::Tokens;
 use core::fmt;
+use std::array;
 
 pub struct AST {}
 
@@ -6,10 +8,14 @@ pub struct SyntaxError {}
 
 impl fmt::Display for SyntaxError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!()
+        write!(f, "syntax error")
     }
 }
 
-pub(crate) fn emit_ast(tokens: Vec<super::lexer::token::Token>) -> Result<AST, SyntaxError> {
-    todo!()
+pub(crate) fn emit_ast(tokens: Tokens) -> Result<AST, SyntaxError> {
+    for t in tokens {
+        println!("{}", t.kind)
+    }
+
+    Ok(AST {})
 }
